4a22a64288f3fd1b88afde68504e9755f5110127
maheshphalke475 <56152702+maheshphalke475@users.noreply.github.com>
2019-10-04 14:57:23 +0530

    Update VOICECHECK.js

--- VOICECHECK.js
+++ VOICECHECK.js
@@ -1,2 +1,2 @@
-var extPath = "//assets.webofwork.com/extensions/talk-to-qlik/auto/";
+var extPath = "//github.com/maheshphalke475/Checkupload.git/";
 define([extPath + "bundle.js", "css!" + extPath + "style.css"], function (b) { return b; });
